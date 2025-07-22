949d89b221032cba863a53eec741be6ed6dbe7d3
rahmandurjoy04 <nainurrahman70@gmail.com>
2025-07-22 21:37:24 +0600

    Replaced BaseURL

--- src/hoooks/useAxios.jsx
+++ src/hoooks/useAxios.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 
 const axiosInstance = axios.create({
-  baseURL: `http://localhost:3000/`
+  baseURL: `https://plate-share-server-rho.vercel.app/`
 });
 const useAxios = () => {
     
